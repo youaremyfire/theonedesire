acf6ff6b404b100b9a8cbec9c3e6093b639cec90
belivewhenisay <e227589@sbl.ch>
2022-11-16 14:51:13 +0100

    hallo

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function handleKeyPress(ev) {
-    console.log(ev)
+    const player = document.querySelector("#player")
+    if (ev.code === "Space") {
+        player.setAttribute("cx", 100 *Math.random())
+        player.setAttribute("cy", 100 * Math.random())
+    } else if (ev.code === "r") {
+        player.setAttribute("fill", "red")
+    } else if (ev.key === "g") {
+        
+    }
 }
 
 
